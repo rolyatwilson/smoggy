e7fdb57a5afec14d2f661ace7e60256c565c45b9
Taylor Wilson <twilson@instructure.com>
2016-08-23 23:17:04 -0600

    Fix missing module declaration

--- lib/smoggy/bounding_box.rb
+++ lib/smoggy/bounding_box.rb
@@ -1,14 +1,16 @@
-class BoundingBox
-  attr_accessor :sw
-  attr_accessor :ne
+module Smoggy
+  class BoundingBox
+    attr_accessor :sw
+    attr_accessor :ne
 
-  def initialize(south_west, north_east)
-    @sw = south_west
-    @ne = north_east
-  end
+    def initialize(south_west, north_east)
+      @sw = south_west
+      @ne = north_east
+    end
 
-  def to_str
-    str = "#{sw.lng},#{sw.lat},#{ne.lng},#{ne.lat}"
-    str
+    def to_str
+      str = "#{sw.lng},#{sw.lat},#{ne.lng},#{ne.lat}"
+      str
+    end
   end
 end
--- lib/smoggy/coordinate.rb
+++ lib/smoggy/coordinate.rb
@@ -1,9 +1,11 @@
-class Coordinate
-  attr_accessor :lat
-  attr_accessor :lng
+module Smoggy
+  class Coordinate
+    attr_accessor :lat
+    attr_accessor :lng
 
-  def initialize(latitude, longitude)
-    @lat = latitude
-    @lng = longitude
+    def initialize(latitude, longitude)
+      @lat = latitude
+      @lng = longitude
+    end
   end
 end
